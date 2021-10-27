c97b32acfa9bc63ead9288e52bb74e7897b30e51
itrepo01 <itrepo01@gmail.com>
2021-10-26 20:32:54 -0700

    add files

---
+++ Week07/Backup-Files.ps1
@@ -0,0 +1,17 @@
+﻿Write-Host "Saving the following files" `
+            "to the `"Backup`" folder:"
+$BUCount = 0
+$BUSize  = 0
+
+Get-ChildItem -File | where {
+    $_.LastWriteTime -ge "2018-01-01" } | 
+ForEach-Object {
+    Write-Host " Backup\$($_.Name)"
+    $BUSize += $_.Length 
+    $BUCount++
+    $FileName = [io.path]::GetFileNameWithoutExtension($_.name)
+    $FileType = [io.path]::GetExtension($_.name)
+    $NewFileName = "Backup\$FileName" + "_" + $_.LastWriteTime.ToString("yyyy-MM-dd") + $FileType
+    Copy-Item $_ $NewFileName }
+
+Write-Host "Saved:" $BUCount "files," $BUSize.ToString("n0") "bytes"
