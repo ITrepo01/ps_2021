bec8eb88e09029cf0d44f4f5565a9ace05c4ecca
ITrepo <itrepo01@gmail.com>
2021-10-26 18:26:00 -0700

    init commit

---
+++ Backup-Files.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host "Saving the following files" `
+            "to the `"Backup`" folder:"
+$BUCount = 0
+$BUSize  = 0
+
+Get-ChildItem -File | where {
+    $_.LastWriteTime -ge "2018-01-01" } | 
+ForEach-Object {
+    Write-Host " Backup\$($_.Name)"
+    $BUSize += $_.Length 
+    $BUCount++
+    $NewFileName = "Backup\" + $_.Name + "_" + `
+        $_.LastWriteTime.ToString("yyyy-MM-dd")
+    Copy-Item $_ $NewFileName }
+
+Write-Host "Saved:" $BUCount "files," $BUSize.ToString("n0") "bytes"
